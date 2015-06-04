15fb093df0b60be3ce2812f5876c0211d70f6cf3
Maurício Moreira de Aguilar <mmaguylar@hotmail.com>
2015-06-03 23:36:30 -0300

    segundo teste commit

--- gincana-solidaria/src/main/java/teste/teste2.java
+++ gincana-solidaria/src/main/java/teste/teste2.java
@@ -2,12 +2,17 @@ package teste;
 
 public class teste2 {
 
-	//qualquer coisa
-	private void testegincana(){
+	// qualquer coisa
+	private void testegincana() {
 		System.out.println("não faz nada");
 	}
-	private String tempo(){		
+
+	private String tempo() {
 		return null;
 	}
-	
+
+	private Boolean mauricio() {
+		return true;
+	}
+
 }
